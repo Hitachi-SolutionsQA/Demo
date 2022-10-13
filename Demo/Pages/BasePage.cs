349825b5854416d7fd17dce4fbba3bd66a92b513
Miguel Acosta <macosta@hitachisolutions.com>
2022-10-13 03:23:00 -0400

    working solution

---
+++ Demo/Pages/BasePage.cs
@@ -0,0 +1,61 @@
+﻿using BoDi;
+using HitachiQA.Driver;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using HitachiQA;
+
+namespace Demo.Pages
+{
+    public class BasePage
+    {
+        public readonly UserActions UserActions;
+        public readonly ScreenShot ScreenShot;
+        public BasePage(ObjectContainer ObjectContainer)
+        {
+            this.UserActions = ObjectContainer.Resolve<UserActions>();
+            this.ScreenShot = ObjectContainer.Resolve<ScreenShot>();
+           
+
+        }
+
+        protected Element Element(string xpath)
+        {
+            return Element(By.XPath(xpath));
+        }
+        protected Element Element(By locator)
+        {
+            return new Element(locator, UserActions);
+        }
+
+        public void ScrollToBottom()
+        {
+            UserActions.ScrollToBottom();
+        }
+
+        public void ScrollToTop()
+        {
+            UserActions.ScrollToTop();
+        }
+
+        public string GetCurrentURL()
+        {
+            return UserActions.GetCurrentURL();
+        }
+
+        public string GetCurrentURLPath()
+        {
+            return new Uri(UserActions.GetCurrentURL()).PathAndQuery;
+        }
+
+        public void refreshPage()
+        {
+            UserActions.Refresh();
+        }
+    }
+}
---
+++ Demo/Pages/HsalHome.cs
@@ -0,0 +1,21 @@
+﻿using BoDi;
+using HitachiQA.Driver;
+
+namespace Demo.Pages
+{
+    public class HsalHome : BasePage
+    {
+        public HsalHome(ObjectContainer OC) : base(OC)
+        {
+            
+        }
+        public string URL_PATH = "/";
+        public void navigate() => UserActions.Navigate(URL_PATH);
+        public Element OpenSearch => Element("//*[@id='SupportNaviSearch']");
+        public Element SearchInput => Element("//*[@id='MF_form_phrase']");
+        public Element SearchButton => Element("//*[@class='SearchBtn']");
+        public Element ResultSearchInput => Element("//input[@title='search query']");
+
+    }
+}
+
